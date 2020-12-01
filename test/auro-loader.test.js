b8d3982210f7608734b49f72fdbb12310a1e8abe
Dale Sande <dale.sande@gmail.com>
2020-11-30 17:02:33 -0800

    test: add custom element tests

--- test/auro-loader.test.js
+++ test/auro-loader.test.js
@@ -11,6 +11,39 @@ describe('auro-loader', () => {
     await expect(el).to.be.accessible();
   });
 
+  it('auro-loader custom element uses inline SVG', async () => {
+    const el = await fixture(html`
+      <auro-loader ringworm></auro-loader>
+    `);
+
+    const root = el.shadowRoot.querySelectorAll('svg');
+    const length = 1;
+
+    await expect(root.length).to.equal(length);
+  });
+
+  it('auro-loader should have two elements', async () => {
+    const el = await fixture(html`
+      <auro-loader laser></auro-loader>
+    `);
+
+    const root = el.shadowRoot.querySelectorAll('span');
+    const length = 2;
+
+    await expect(root.length).to.equal(length);
+  });
+
+  it('auro-loader should have three elements', async () => {
+    const el = await fixture(html`
+      <auro-loader pulse></auro-loader>
+    `);
+
+    const root = el.shadowRoot.querySelectorAll('span');
+    const length = 3;
+
+    await expect(root.length).to.equal(length);
+  });
+
   it('auro-loader custom element is defined', async () => {
     const el = await !!customElements.get("auro-loader");
 

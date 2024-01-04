ad0c50d43431508ca192a71ee873e548e7f2ce63
Dale Sande <dale.sande@gmail.com>
2024-01-03 17:53:50 -0800

    refactor(generator migration): migrate repo to latest generator build

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-export * from './dist/auro-loader';
+import { AuroLoader } from './src/auro-loader';
+
+/**
+ * Register Custom Element.
+ * @param {Object} name - Name to use for custom element.
+ * @returns {void}
+ */
+ const registerComponent = (name = 'custom-loader') => {
+  // alias definition
+  if (!customElements.get(name)) {
+    customElements.define(name, class extends AuroLoader {});
+  }
+}
+
+export { registerComponent }
